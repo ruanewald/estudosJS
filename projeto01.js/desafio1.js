8099bd86cec53d128c6b503b8cb0e89f1609ed15
ruanewald <ruan.devv@gmail.com>
2025-02-28 17:12:59 -0300

    Enviando tudo

---
+++ projeto01.js/desafio1.js
@@ -0,0 +1,13 @@
+// Relembrando coisa que já vi antes
+
+function somar(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+const somar2 = (numero3, numero4) => {
+    return numero3 + numero4;
+}
+
+console.log(somar(1, 200))
+console.log(somar2(2, 200))
+
